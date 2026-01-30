e65c850130c24789475ade28f310efc5d3aeb863
alexavonic <alex.van.hooijdonk@avonic.com>
2026-01-30 20:55:58 +0100

    added konva svelte plugin with demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Stage, Layer, Rect } from 'svelte-konva';
+
+	let width = $state(0);
+	let height = $state(0);
+
+	let rectX = $state(100);
+	let rectY = $state(100);
+
+	const rectWidth = 400;
+	const rectHeight = 200;
+
+	function dragBound(pos: { x: number; y: number }) {
+		return {
+			x: Math.max(0, Math.min(pos.x, width - rectWidth)),
+			y: Math.max(0, Math.min(pos.y, height - rectHeight))
+		};
+	}
+</script>
+
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+
+<div class="h-screen w-full border-2 border-dashed border-gray-400">
+	<Stage {width} {height}>
+		<Layer>
+			<Rect
+				bind:x={rectX}
+				bind:y={rectY}
+				width={rectWidth}
+				height={rectHeight}
+				fill="blue"
+				draggable
+				dragBoundFunc={dragBound}
+			/>
+		</Layer>
+	</Stage>
+</div>
+
+<!-- Move to app.css -->
+<style>
+	:global(html),
+	:global(body),
+	:global(#svelte) {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+	}
+</style>
